9481395e21391b5de04ff7c9280f4f42afe082a2
Shay Maor <shay.h.maor@gmail.com>
2019-12-05 15:32:12 -0500

    Attachments by Query and Send Email

---
+++ GMAIL/AttachmentsByQuery.py
@@ -0,0 +1,76 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import base64
+import email
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+
+def GetAttachments(service, user_id, msg_id):
+    """Get and store attachment from Message with given id.
+
+    :param service: Authorized Gmail API service instance.
+    :param user_id: User's email address. The special value "me" can be used to indicate the authenticated user.
+    :param msg_id: ID of Message containing attachment.
+    """
+    #try:
+    message = service.users().messages().get(userId=user_id, id=msg_id).execute()
+    try:
+        for part in message['payload']['parts']:
+            if part['filename']:
+                if 'data' in part['body']:
+                    data = part['body']['data']
+                else:
+                    att_id = part['body']['attachmentId']
+                    att = service.users().messages().attachments().get(userId=user_id, messageId=msg_id,id=att_id).execute()
+                    data = att['data']
+                file_data = base64.urlsafe_b64decode(data.encode('UTF-8'))
+                path = part['filename']
+
+                f = open(path, 'wb')
+                f.write(file_data)
+                f.close()
+    except:
+        print('An error occurred')
+
+
+def main():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    query = "from:test@test.com" # Enter your query here
+
+    # Call the Gmail API
+    results = service.users().messages().list(userId="me",
+                                               q=query).execute()
+
+
+    for message_i in results["messages"]:
+        GetAttachments(service,"me",message_i["id"])
+    
+
+if __name__ == '__main__':
+    main()
